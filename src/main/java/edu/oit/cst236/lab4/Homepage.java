31796c6c6fa8a4a10995723a62724ea55b2764f5
Nick Ferraro <spidy.88@gmail.com>
2014-03-05 17:47:27 -0800

    Updating webpage

--- src/main/java/edu/oit/cst236/lab4/Homepage.java
+++ src/main/java/edu/oit/cst236/lab4/Homepage.java
@@ -16,7 +16,11 @@ public class Homepage extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("Welcome to Lab 4");
+		resp.getWriter().print(getWebPageFor(getName()));
+	}
+	
+	public String getName() {
+		return "Nick Ferraro";
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -27,5 +31,9 @@ public class Homepage extends HttpServlet {
 		context.addServlet(new ServletHolder(new Homepage()), "/*");
 		server.start();
 		server.join();
+	}
+	
+	public final static String getWebPageFor(String name) {
+		return "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"generator\" content=\"HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39\" /><meta charset=\"utf-8\" /><title>Lab 4 - Complete</title><link rel=\"stylesheet\" type=\"text/css\" href=\"http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.0.4/css/bootstrap.min.css\" /><link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'><link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'><style>html, body {height: 100%;text-align: center;} h1 {font-family: 'Rock Salt', cursive;font-size: 46px;line-height: 50px;} h3 {font-family: 'Rock Salt', cursive;font-size: 36px;line-height: 40px;} .name {font-family: 'Open Sans', sans-serif;font-size: 72px;line-height: 76px;margin-top: 40px;margin-bottom: 40px;color: blue;} .panel {position: relative;height: 250px;width: 900px;top: 50%;left: 50%;margin-top: -125px;margin-left: -450px;}</style><script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></script><script type=\"text/javascript\"></script></head><body><div class=\"panel\"><span><h1>Congratulations</h1><div class=\"name\">" + name + "</div><h3>You Have Successfully Completed Lab 4</h3></span></div></body></html>";
 	}
 }
