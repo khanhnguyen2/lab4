dffb76e9bdaa8ce7a7606171134d33dcfec1d85e
khanhnguyen2 <khanh.nguyen2@oit.edu>
2014-03-14 08:07:23 -0700

    Update Homepage.java

--- src/main/java/edu/oit/cst236/lab4/Homepage.java
+++ src/main/java/edu/oit/cst236/lab4/Homepage.java
@@ -20,7 +20,7 @@ public class Homepage extends HttpServlet {
 	}
 	
 	public String getName() {
-		return "";
+		return "Khanh8173";
 	}
 
 	public static void main(String[] args) throws Exception {
